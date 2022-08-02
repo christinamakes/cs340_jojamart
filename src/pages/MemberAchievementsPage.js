cd438446cd04f00f8d176580173ddb0d43cc74fa
Christina Moore <sayhitochristina@gmail.com>
2022-08-02 10:05:12 -0600

    set up member achievement for GET

--- src/pages/MemberAchievementsPage.js
+++ src/pages/MemberAchievementsPage.js
@@ -6,12 +6,27 @@ import Footer from '../components/Footer';
 import Navigation from '../components/NavBar';
 import MemberAchievementsList from '../components/MemberAchievementsList';
 import { Link } from 'react-router-dom';
+import { useState, useEffect} from 'react';
 
 function MemberAchievementsPage() {
     // Data hardcoded for now, dynamic later
     const memAchList = [{'id': 22, 'achievementId': 2, 'memberId': 2},
                     {'id': 32, 'achievementId': 3, 'memberId': 2},
                     {'id': 31, 'achievementId': 3, 'memberId': 1}]
+
+    const [memAchs, setMemAchs] = useState([])
+
+    // get members from /members
+    const loadMemAchs = async () => {
+        const response = await fetch(`${URL}/member-achievements`);
+        const memAchs = await response.json();
+        setMemAchs(memAchs);
+    }
+
+    useEffect(() => {
+        loadMemAchs();
+    }, []);
+
     return (
         <div>
             <header>
