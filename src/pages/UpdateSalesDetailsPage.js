f639ed3ac4f11adad3d27d283c50a8cd1dbebdc5
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2022-08-07 15:55:45 -0400

    lets goooo

--- src/pages/UpdateSalesDetailsPage.js
+++ src/pages/UpdateSalesDetailsPage.js
@@ -7,15 +7,15 @@ const URL = 'https://joja-server.herokuapp.com'
 
 export const UpdateSalesDetailsPage = ({salesDetailToEdit}) => {
     const navigate = useNavigate();
-    const [product_id, setProductId] = useState('');
-    const [order_number, setOrderNumber] = useState('');
-    const [quantity, setQuantity] = useState('');
-    const [order_type, setOrderType] = useState('');
+    const [product_id, setProductId] = useState(salesDetailToEdit);
+    const [order_number, setOrderNumber] = useState(salesDetailToEdit);
+    const [quantity, setQuantity] = useState(salesDetailToEdit);
+    const [order_type, setOrderType] = useState(salesDetailToEdit);
     // For Sales
-    const [member_id, setMember] = useState('');
-    const [employee_id, setEmployee] = useState('');
-    const [purchase_date, setDate] = useState('');
-    const [invoice_total, setTotal] = useState('');
+    const [member_id, setMember] = useState(salesDetailToEdit);
+    const [employee_id, setEmployee] = useState(salesDetailToEdit);
+    const [purchase_date, setDate] = useState(salesDetailToEdit);
+    const [invoice_total, setTotal] = useState(salesDetailToEdit);
 
     // For Dropdowns
     const [members, setMembers] = useState([]);
@@ -82,11 +82,6 @@ export const UpdateSalesDetailsPage = ({salesDetailToEdit}) => {
         }
     };
 
-    const updateLineItem = () => {
-        updateSale();
-        updateSalesDetail();
-    }
-
     useEffect(() => {
         loadMembers();
         loadEmployees();
@@ -103,56 +98,63 @@ export const UpdateSalesDetailsPage = ({salesDetailToEdit}) => {
             </header>
             <p>Update a Sales Details Instance on this page</p>
             <div>
-                <label htmlFor='uSaleMember'>Member ID: </label>
-                <select id='uSaleMember' name='member_id' onChange={e => setMember(e.target.value)}>
-                    <option value='Select a Member'> -- Select a Member -- </option>
-                    {members.map((member, i) => <option value={member.member_id} key={i}>{member.member_id + ' -- ' + member.member_name}</option>)}
-                </select>
-                <label htmlFor='uSaleEmployee'>Employee ID: </label>
-                <select id='uSaleEmployee' name='employee_id' onChange={e => setEmployee(e.target.value)}>
-                    <option value='Select an Employee'> -- Select an Employee -- </option>
-                    <option value='0'>None</option>
-                    {employees.map((employee, i) => <option value={employee.employee_id} key={i}>{employee.employee_id + ' -- ' + employee.employee_name}</option>)}
-                </select>
-                <label htmlFor='uSaleDate'>Date: </label>
-                <input
-                    id='uSaleDate'
-                    type="text"
-                    placeholder="Enter date here"
-                    value={purchase_date}
-                    onChange={e => setDate(e.target.value)} />
-                <label htmlFor='uSaleTotal'>Invoice Total: </label>
-                <input
-                    id='uSaleTotal'
-                    type="text"
-                    placeholder="Enter invoice total here"
-                    value={invoice_total}
-                    onChange={e => setTotal(e.target.value)} />
-                <label htmlFor='uSaleDetProd'>Product ID: </label>
-                <select id='uSaleDetProd' name='product_id' onChange={e => setProductId(e.target.value)}>
-                    <option value='Select a Product'> -- Select a Product -- </option>
-                    {products.map((product, i) => <option value={product.product_id} key={i}>{product.product_id + ' -- ' + product.product_name}</option>)}
-                </select>
-                <label htmlFor='uSaleDetOrd'>Order Number: </label>
-                <select id='uSaleDetOrd' name='order_number' onChange={e => setOrderNumber(e.target.value)}>
-                <option value='Select an Order Number'> -- Select an Order Number -- </option>
-                {sales.map((sale, i) => <option value={sale.order_number} key={i}>{sale.order_number}</option>)}
-                </select>
-                <label htmlFor='uSaleDetQuant'>Quantity: </label>
-                <input
-                    id='uSaleDetQuant'
-                    type="text"
-                    placeholder="Enter quantity here"
-                    value={quantity}
-                    onChange={e => setQuantity(e.target.value)} />
-                <label htmlFor='uSaleDetOrdType'>Order Type: </label>
-                <select id='uSaleDetOrdType' name='order_type' onChange={e => setOrderType(e.target.value)}>
-                    <option value='0'>0 - In Person</option>
-                    <option value='1'>1 - Online</option>
-                </select>
-                <button
-                    onClick={updateLineItem}
-                    >Update</button>
+                <div>
+                    <label htmlFor='uSaleMember'>Member ID: </label>
+                    <select id='uSaleMember' name='member_id' onChange={e => setMember(e.target.value)}>
+                        <option value='Select a Member'> -- Select a Member -- </option>
+                        {members.map((member, i) => <option value={member.member_id} key={i}>{member.member_id + ' -- ' + member.member_name}</option>)}
+                    </select>
+                    <label htmlFor='uSaleEmployee'>Employee ID: </label>
+                    <select id='uSaleEmployee' name='employee_id' onChange={e => setEmployee(e.target.value)}>
+                        <option value='Select an Employee'> -- Select an Employee -- </option>
+                        <option value='0'>None</option>
+                        {employees.map((employee, i) => <option value={employee.employee_id} key={i}>{employee.employee_id + ' -- ' + employee.employee_name}</option>)}
+                    </select>
+                    <label htmlFor='uSaleDate'>Date: </label>
+                    <input
+                        id='uSaleDate'
+                        type="text"
+                        placeholder="Enter date here"
+                        value={purchase_date}
+                        onChange={e => setDate(e.target.value)} />
+                    <label htmlFor='uSaleTotal'>Invoice Total: </label>
+                    <input
+                        id='uSaleTotal'
+                        type="text"
+                        placeholder="Enter invoice total here"
+                        value={invoice_total}
+                        onChange={e => setTotal(e.target.value)} />
+                    <label htmlFor='uSaleDetProd'>Product ID: </label>
+                    <select id='uSaleDetProd' name='product_id' onChange={e => setProductId(e.target.value)}>
+                        <option value='Select a Product'> -- Select a Product -- </option>
+                        {products.map((product, i) => <option value={product.product_id} key={i}>{product.product_id + ' -- ' + product.product_name}</option>)}
+                    </select>
+                    <button
+                        onClick={updateSale}
+                        >Update</button>
+                </div>
+                <div>
+                    <label htmlFor='uSaleDetOrd'>Order Number: </label>
+                    <select id='uSaleDetOrd' name='order_number' onChange={e => setOrderNumber(e.target.value)}>
+                    <option value='Select an Order Number'> -- Select an Order Number -- </option>
+                    {sales.map((sale, i) => <option value={sale.order_number} key={i}>{sale.order_number}</option>)}
+                    </select>
+                    <label htmlFor='uSaleDetQuant'>Quantity: </label>
+                    <input
+                        id='uSaleDetQuant'
+                        type="text"
+                        placeholder="Enter quantity here"
+                        value={quantity}
+                        onChange={e => setQuantity(e.target.value)} />
+                    <label htmlFor='uSaleDetOrdType'>Order Type: </label>
+                    <select id='uSaleDetOrdType' name='order_type' onChange={e => setOrderType(e.target.value)}>
+                        <option value='0'>0 - In Person</option>
+                        <option value='1'>1 - Online</option>
+                    </select>
+                    <button
+                        onClick={updateSalesDetail}
+                        >Update</button>
+                </div>
             </div>
             <div>
                 <Footer />
