9b00219798a73b6b84ae5843b410b8ccff9562d9
Christina Moore <sayhitochristina@gmail.com>
2022-08-07 16:26:25 -0600

    delete mem_achs

--- src/components/MemberAchievement.jsx
+++ src/components/MemberAchievement.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { AiOutlineDelete } from "react-icons/ai";
+import PropTypes from 'prop-types';
 
-function MemberAchievement({ memberAchievement, onDelete}) {
+function MemberAchievement({ memberAchievement, onDelete }) {
+    MemberAchievement.propTypes = {
+        memberAchievement: PropTypes.array,
+        onDelete: PropTypes.func,
+    }
     return (
         <tr>
-            <td>{memberAchievement.achievement_id}</td>
-            <td>{memberAchievement.member_id}</td>
+            <td>{memberAchievement.achievement_id} -- {memberAchievement.achievement_title}</td>
+            <td>{memberAchievement.member_id} -- {memberAchievement.member_name}</td>
             <td className='click'><AiOutlineDelete onClick={() => onDelete(memberAchievement.member_id)} /></td>
         </tr>
     );
--- src/pages/MemberAchievementsPage.jsx
+++ src/pages/MemberAchievementsPage.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-// import { useState, useEffect } from 'react';
-// import { useHistory, Link } from 'react-router-dom';
-
 import Footer from '../components/Footer';
 import Navigation from '../components/NavBar';
 import MemberAchievementsList from '../components/MemberAchievementsList';
 import { Link } from 'react-router-dom';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function MemberAchievementsPage() {
-    // Data hardcoded for now, dynamic later
- 
-
     const [memAchs, setMemAchs] = useState([])
-
     const URL = 'https://joja-server.herokuapp.com'
 
     // get members from /members
@@ -22,6 +15,21 @@ function MemberAchievementsPage() {
         const memAchs = await response.json();
         setMemAchs(memAchs);
     }
+
+    const onDelete = async (member_id) => {
+        const response = await fetch(`${URL}/member-achievements/delete/${member_id}`, { method: 'DELETE' });
+        if (response.status === 200) {
+            const fetchData = async () => {
+                const response = await fetch(`${URL}/member-achievements/`, { method: 'GET' });
+                const json = await response.json();
+                setMemAchs(json)
+            }
+            fetchData();
+        } else {
+            alert(`Failed to delete member`)
+        }
+    }
+
 
     useEffect(() => {
         loadMemAchs();
@@ -36,7 +44,7 @@ function MemberAchievementsPage() {
             <div>
                 <p>These are the achievements of our valued members. Members with more achievements are, of course, more valued by me. I mean the company.</p>
             </div>
-            <MemberAchievementsList memberAchievements={memAchs}></MemberAchievementsList>
+            <MemberAchievementsList memberAchievements={memAchs} onDelete={onDelete} />
             <div className='add'>
                 <ul>
                     <li>
